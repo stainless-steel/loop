28f4226dfa22565c471002a10fa697df30976eba
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-12-28 11:01:19 +0100

    Fix a typo

--- src/support.rs
+++ src/support.rs
@@ -1,6 +1,6 @@
 pub fn workers(value: Option<usize>) -> usize {
     value
-        .map(|value| std::cmp::min(value, 1))
+        .map(|value| std::cmp::max(value, 1))
         .unwrap_or_else(|| {
             std::thread::available_parallelism()
                 .map(|value| value.get())
